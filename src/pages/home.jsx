cc0da9474897c7c575f6e8ab24d03cd91bc2a9c4
keennathan <keennathan@icloud.com>
2025-01-01 01:05:05 +1000

    feat: changed dependency react-typical to react-simple-typewriter, because it was not compatable with version of react

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,30 +1,37 @@
-import React from "react";
-import Typical from "react-typical";
+import React, { useState, useEffect } from "react";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 import styles from "../cssModules/home.module.css";
 
 const Home = () => {
+    const [text] = useTypewriter({
+        words: ["Front-End Developer", "Back-End Developer", "Full Stack Developer"],
+        loop: 10,
+        cursor: true,
+        cursorStyle: "|",
+        typeSpeed: 100,
+        deleteSpeed: 80,
+        delaySpeed: 2000,
+    });
     return (
         <section id="home" className="section-home pt-5">
-            <div className={styles.content}> 
+            <div className={styles.content}>
                 <div className={styles.imageContainer}>
-                    <img id="profileImage" className={styles.profileImage} src="/assets/profile.JPG" alt="Profile Picture" />
+                    <img
+                        id="profileImage"
+                        className={styles.profileImage}
+                        src="/assets/profile.JPG"
+                        alt="Profile Picture"
+                    />
                 </div>
                 <div className={styles.textContainer}>
                     <h1>
-                        <Typical
-                            steps={[
-                                "Hi, I'm Nathan Keen Graduate Front-End", 1000,
-                                "Hi, I'm Nathan Keen Graduate Back-End", 1000,
-                                "Hi, I'm Nathan Keen Graduate Full Stack Developer.", 1000
-                            ]}
-                            loop={1}
-                            wrapper="p"
-                        />
+                        <span>Hi, I'm Nathan Keen </span>
+                        <span>I'm a <span style={{color:"blue"}}>{text}</span><Cursor /></span>
                     </h1>
                 </div>
             </div>
         </section>
     );
-}
+};
 
 export default Home;
